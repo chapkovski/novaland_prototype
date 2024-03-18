b88dfab7bc9552b9f407b12d6aecdbbc6744b161
Chapkovski <chapkovski@MacBook-Pro-von-gw-admin.local>
2024-03-18 12:45:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app app>
+    <v-main app>
+      <h1>Welcome to Novaland</h1>
+      <v-sheet> 
+        
+        
+      </v-sheet>  
+      <SurveyComponent :survey="survey"></SurveyComponent>
+      </v-main  ></v-app>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { Model } from "survey-core";
+import "./index.css";
+import { json } from "./json";
+
+const survey = new Model(json);
+// You can delete the line below if you do not use a customized theme
+
+survey.onComplete.add((sender, options) => {
+  console.log(JSON.stringify(sender.data, null, 3));
+});
+survey.data = {
+  "nps-score": 9,
+  "promoter-features": ["performance", "ui"],
+};
+
+const msg = ref("World");
+</script>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+
+import { surveyPlugin } from "survey-vue3-ui";
+
+import "survey-core/defaultV2.min.css";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(vuetify).use(router).use(surveyPlugin).mount("#app");
